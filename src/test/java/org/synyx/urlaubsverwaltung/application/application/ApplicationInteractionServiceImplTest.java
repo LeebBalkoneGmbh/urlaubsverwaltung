e0b4971fe08ed077ceea035150cede507c835a78
Joel Strasser <j.strasser@leeb-balkone.com>
2025-10-23 17:02:39 +0200

    fix: add missing param in ApplicationInteractionServiceImplTest

--- src/test/java/org/synyx/urlaubsverwaltung/application/application/ApplicationInteractionServiceImplTest.java
+++ src/test/java/org/synyx/urlaubsverwaltung/application/application/ApplicationInteractionServiceImplTest.java
@@ -92,7 +92,7 @@ class ApplicationInteractionServiceImplTest {
     @BeforeEach
     void setUp() {
         sut = new ApplicationInteractionServiceImpl(applicationService, commentService, accountInteractionService,
-            applicationMailService, departmentService, clock, applicationEventPublisher);
+            applicationMailService, departmentService, clock, applicationEventPublisher, null);
     }
 
     // APPLY FOR LEAVE -------------------------------------------------------------------------------------------------
